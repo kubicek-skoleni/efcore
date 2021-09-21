e89e2f38992a44b17f992d55428371f77ba529c7
kubicek-skoleni <kubicek.brno@gmail.com>
2021-09-21 12:08:15 +0200

    Consolova aplikace BankIS - class a instance

---
+++ BankIS/BankIS/BankIS.ConsoleApp/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankIS.ConsoleApp
+{
+    public class Client
+    {
+        public string Name { get; set; }
+
+        public int Age {  get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
