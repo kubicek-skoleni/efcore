6b56515783ce011d1c1a32a261501857d3a9404a
kubicek-skoleni <kubicek.brno@gmail.com>
2021-09-21 14:21:40 +0200

    lze priradit c2 a c1 na stejnou instanci

--- BankIS/BankIS/BankIS.ConsoleApp/Program.cs
+++ BankIS/BankIS/BankIS.ConsoleApp/Program.cs
@@ -14,14 +14,17 @@ namespace BankIS.ConsoleApp
             ostrava.City = "Ostrava";
             ostrava.Street = "Tovární 14";
 
-            Client c1 = new Client();
+            Client c1;
+            c1 = new Client();
             c1.Name = "Jan Novák";
             c1.Age = 31;
             c1.HomeAddress = ostrava;
 
-            Client c2 = new Client();
+            Client c2;
+            c2 = c1;
             c2.Name = "Martina Veselá";
             c2.Age = 33;
+            c2.HomeAddress = ostrava;
             
 
             c1.Print();
