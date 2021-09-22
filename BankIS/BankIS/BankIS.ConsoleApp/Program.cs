3ec35d0b3c5698012b788ecceea2c9cc3749f234
kubicek-skoleni <kubicek.brno@gmail.com>
2021-09-22 09:25:37 +0200

    5 klientů

--- BankIS/BankIS/BankIS.ConsoleApp/Program.cs
+++ BankIS/BankIS/BankIS.ConsoleApp/Program.cs
@@ -13,55 +13,10 @@ namespace BankIS.ConsoleApp
 
             var c1 = new Client(city: "Brno", street: "Kopečná 23", jmeno: "Martin Novák", age: 55);
             var c2 = new Client("Tovární 13", "Ostrava", "Martina Veselá", 33);
+            var c3 = new Client("Česká 1", "Brno", "Karel Novotný", 30);
+            var c4 = new Client("Poštovní 3", "Praha", "Jitka", 43);
+            var c5 = new Client("Hluchá 13", "Plzeň", "Jiří Ondra", 53);
 
-            // připravte metodu "SaveToFile(string filePath) na klientovi
-
-            Console.WriteLine("c1: " + c1.ToString());
-
-            c1.Print();
-
-            Console.WriteLine();
-            var num = int.Parse("32");
-
-            var spojene = string.Join(", ", "Slovo1", "Slovo2", "tři", "atd..");
-            Console.WriteLine(spojene);
-            
-            c2.Print();
-
-            c1.SaveToFile("klient1.txt");
-            c2.SaveToFile("klient2.txt");
-
-            bool allowed = Address.IsCityAllowed("xxx");
-
-            double cos = Math.Cos(3.2);
-
-            using (HttpClient webClient = new HttpClient())
-            {
-                var response = webClient.GetAsync("https://API.projekt.cz").Result;
-                if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                {
-                    //response OK HTTP 200
-                }
-            }
-
-            string[] pole_stringu = new string[23];
-
-            List<string> list = new List<string>();
-
-            pole_stringu[5] = "sesty prvek";
-            
-            list.Add("prvni item");
-
-            List<Client> clients = new List<Client>();
-
-            clients.Add(c1);
-            clients.Add(c2);
-
-            foreach(var client in clients)
-            {
-                client.Age = 22;
-                client.Print();
-            }
 
         }
 
