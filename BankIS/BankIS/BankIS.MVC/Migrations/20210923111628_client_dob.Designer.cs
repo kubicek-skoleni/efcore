0445771c2e56b6a64c816d551f0011e727f4deaa
kubicek-skoleni <kubicek.brno@gmail.com>
2021-09-23 13:18:57 +0200

    migrace

--- BankIS/BankIS/BankIS.MVC/Migrations/20210923111628_client_dob.Designer.cs
+++ BankIS/BankIS/BankIS.MVC/Migrations/20210923111628_client_dob.Designer.cs
@@ -4,14 +4,16 @@ using BankIS.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankIS.MVC.Migrations
 {
     [DbContext(typeof(BankContext))]
-    partial class BankContextModelSnapshot : ModelSnapshot
+    [Migration("20210923111628_client_dob")]
+    partial class client_dob
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BankIS/BankIS/BankIS.MVC/Migrations/20210923111628_client_dob.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankIS.MVC.Migrations
+{
+    public partial class client_dob : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Clients",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Clients",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Clients");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
