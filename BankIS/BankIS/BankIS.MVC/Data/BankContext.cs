45bb951ec269163c3b49e659099aab51098d9c60
kubicek-skoleni <kubicek.brno@gmail.com>
2021-09-23 11:16:56 +0200

    bank context

---
+++ BankIS/BankIS/BankIS.MVC/Data/BankContext.cs
@@ -0,0 +1,19 @@
+﻿using BankIS.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankIS.MVC.Data
+{
+    public class BankContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Initial Catalog=bankdb-mvc");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
